2a89afc268ee2dbb733e78800d2b90dc8cab09bd
Adrian Toncean <adrianton3@gmail.com>
2016-10-09 20:38:15 +0200

    Make the outline smooth

--- src/draw.js
+++ src/draw.js
@@ -19,9 +19,15 @@
 				if (value < 0.7) {
 					gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 				} else if (value < 0.9) {
-					gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+					float smooth = smoothstep(0.8, 0.9, value);
+					vec3 from = vec3(0.0);
+					vec3 to = vec3(1.0);
+					gl_FragColor = vec4(mix(from, to, smooth), 1.0);
 				} else {
-					gl_FragColor = vec4(0.2, 0.5, 1.0, 1.0);
+					float smooth = smoothstep(0.9, 1.0, value);
+					vec3 from = vec3(1.0);
+					vec3 to = vec3(0.2, 0.6, 1.0);
+					gl_FragColor = vec4(mix(from, to, smooth), 1.0);
 				}
 			}
 		`
