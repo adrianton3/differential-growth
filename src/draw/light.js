9af02e742a1ab5778140958075d9b2a2ce01c748
Adrian Toncean <adrianton3@gmail.com>
2016-10-23 21:22:10 +0200

    Add basic lighting

--- src/draw/light.js
+++ src/draw/light.js
@@ -22,7 +22,20 @@
 				float deltaNeighbors = 1.0 + (4.0 * current) - up - left - down - right;
 				float ao = pow(smoothstep(0.2, 1.0, deltaNeighbors), 2.0) * smoothstep(0.5, 1.0, current);
 
-				gl_FragColor = vec4(color.rgb * ao, 1.0);
+				vec3 deltaX = normalize(vec3(2.0, 0.0, right - left));
+				vec3 deltaY = normalize(vec3(0.0, 2.0, down - up));
+				vec3 normal = normalize(cross(deltaX, deltaY));
+
+				// for whatever reason vTextureCoord goes up to 0.5 only
+				const vec2 center = vec2(0.25, 0.25);
+				const vec3 light = vec3(center, 1.3);
+
+				vec3 fragment = vec3(vTextureCoord, current);
+				float diffuse = max(0.0, dot(normal, normalize(light - fragment)));
+
+				float specular = pow(diffuse, 20.0);
+
+				gl_FragColor = vec4(color.rgb * (diffuse * 0.8 + specular) * ao, 1.0);
 			}
 		`
 
